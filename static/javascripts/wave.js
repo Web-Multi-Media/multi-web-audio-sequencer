594c8e9894e6d9e4bf5e993631cf7748c202c94f
xavierfav <xavier.favory@gmail.com>
2018-01-04 11:43:04 +0100

    Add regions wavesurfer

--- static/javascripts/wave.js
+++ static/javascripts/wave.js
@@ -1,14 +1,31 @@
 function Wave() {
   this.soundBuffer = null;
   this.wavesurfer = null;
+  this.currentSound = null;
 }
 
 Wave.prototype.init = function() {
   this.wavesurfer = WaveSurfer.create({
-    container: '#waveform'
+    cursorWidth: 0,
+    container: '#waveform',
+    waveColor: 'gray',
+    progressColor: 'gray',
+    maxCanvasWidth: 100,
+    pixelRatio: 1,
   });
 };
 
 Wave.prototype.load = function(soundUrl) {
- this.wavesurfer.load(soundUrl);
+  var wavesurfer = this.wavesurfer;
+  wavesurfer.load(soundUrl);
+  wavesurfer.on('ready', function() {
+    wavesurfer.addRegion({
+      start: 0,
+      end: wavesurfer.getDuration(),
+      color: 'hsla(400, 100%, 30%, 0.1)',
+    });
+    wavesurfer.on('region-updated', function(obj){
+      console.log(obj.start, obj.end);
+    });
+  });
 }
