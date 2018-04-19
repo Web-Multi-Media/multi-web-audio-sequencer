fbd536abe779a8a2ae7f5ad1c613840e4af0adce
xavierfav <xavier.favory@gmail.com>
2018-04-19 16:24:42 +0200

    Remove merge char

--- static/javascripts/search.js
+++ static/javascripts/search.js
@@ -1,73 +1,10 @@
 // FREESOUND SEARCH
 function initSearch() {
-<<<<<<< HEAD
     var search = new Search();
     search.setToken();
     search.addButtonEvents();
     return search;
   }
-  
-  function Search() {
-    var query = null;
-    var page = null;
-    var numPages = null;
-    var numSounds = null;
-    var sliderValue = null;
-  }
-  
-  Search.prototype.setToken = function() {
-    freesound.setToken("bs5DQrWNL9d8zrQl0ApCvcQqwg0gg8ytGE60qg5o");
-  };
-  
-  Search.prototype.freesoundIframe = function(soundId) {
-    return '<iframe frameborder="0" scrolling="no" src="https://freesound.org/embed/sound/iframe/' + soundId + '/simple/small/" width="375" height="30"></iframe>';
-  };
-  
-  Search.prototype.searchFreesound = function(query, page, filter) {
-    var self = this;
-    
-    self.query = query;
-    self.page = page;
-    self.filter = filter;
-    var sort = "rating_desc";
-    freesound.textSearch(query, {
-        page: page,
-        filter: filter,
-        sort: sort,
-        fields: 'id,name,url,previews,analysis',
-        descriptors: 'rhythm.onset_times' 
-      },
-      function (sounds) {
-        var msg = ""
-        self.numSounds = sounds.count;
-        self.numPages = Math.ceil(self.numSounds/15);
-        var numSoundCurrentPage = sounds.results.length;
-        for (i = 0; i < numSoundCurrentPage; i++) {
-          var snd = sounds.getSound(i);
-          var onsets = snd.analysis.rhythm.onset_times;
-          var firstOnset;
-          if (onsets instanceof Array) {
-            firstOnset = onsets[0];
-          } else {
-            firstOnset = onsets;
-          }
-          console.log(onsets)
-          msg += "<div>" + self.freesoundIframe(snd.id) 
-            + "<div class='drag-me' draggable='true' ondragstart='drag(event)' sound-url='" 
-            + snd.previews["preview-lq-mp3"] 
-            + "' first-onset='" 
-            + firstOnset
-            + "'>Drag</div></div>";
-        }
-        msg += "</ul>"
-        document.getElementById("search-result-container").innerHTML = msg;
-        $('#page').html(self.page+'/' + self.numPages);
-=======
-  var search = new Search();
-  search.setToken();
-  search.addButtonEvents();
-  return search;
-}
 
 function Search() {
   var query = null;
